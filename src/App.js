9423913a31f226d78d76c687801099a13d0fb656
Amna-Amna <mailtoamna4@gmail.com>
2022-08-04 17:43:48 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/seach-box/search-box.component";
+//  import logo from './logo.svg';
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchString: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchString = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchString };
+    });
+  };
+
+  render() {
+    console.log("render from App.js");
+    const { monsters, searchString } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchString);
+    });
+
+    return (
+      <div className="App">
+        <SearchBox
+        className='search-box'
+          onChangeHandler={onSearchChange}
+          placeholder="search Monsters"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
